60b731b4cc4d8ee37a10c9f8fec034a556c63b71
Salih <salihugur46@gmail.com>
2022-10-10 14:41:54 +0300

    header created

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header className='sticky top-0 '>
+        
+    </header>
+  )
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -12,8 +13,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h5 className='text-red-600 text-3xl'>Dent Carreer</h5>
 
+       {/* Header */}
+       <Header/>
     </div>
   )
 }
