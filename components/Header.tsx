266dbf8db5719afd5e4615b3c0a5028139cc7eb3
Salih <salihugur46@gmail.com>
2022-10-10 16:48:12 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { MagnifyingGlassIcon, Cog8ToothIcon, BellIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon, Cog8ToothIcon, BellIcon, Bars3Icon } from '@heroicons/react/24/outline'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <header className='flex sticky top-0 bg-white-200/50 border-b-2 border-gray-100 p-4 justify-between items-center'>
+    <header className='flex sticky top-0 bg-white-200/50 border-b-2 border-gray-100 p-4 justify-between items-center px-10'>
       <div className='flex items-center font-semibold'>
-        <span className='text-xl font-black'>•</span>
-        <span className='text-l ml-2 font-mono'>Dent Carreer</span>
-        <ul className='hidden sm:flex font-semibold text-xs gap-4 ml-8 tracking-wide'>
+        <span className='text-xl lg:text-2xl font-black'>•</span>
+        <span className='text-l lg:text-xl ml-2 font-mono'>Dent Carreer</span>
+
+        {/* TODO: Menu Button for phone screens */}
+        <ul className='hidden md:flex font-semibold text-xs gap-4 lg:gap-10 lg:ml-12 lg:text-sm ml-8 tracking-wide'>
           <li>
             <div className='group cursor-pointer flex flex-col relative h-full items-center'>
               Anasayfa
@@ -37,13 +39,17 @@ const Header = (props: Props) => {
         </ul>
       </div>
 
-      <div className='flex items-center gap-4'>
-        <MagnifyingGlassIcon className='h-6 w-6 text-gray-600 cursor-pointer'/>
-        <Cog8ToothIcon className='h-6 w-6 text-gray-600 cursor-pointer' />
-        <BellIcon className='h-6 w-6 text-gray-600 cursor-pointer'/>
-        <div className='h-8 w-8 rounded-full bg-red-200'>
+      <div className='items-center gap-4 hidden md:flex'>
+        <MagnifyingGlassIcon className='h-4 w-4 md:h-6 md:w-6 text-gray-600 cursor-pointer' />
+        <Cog8ToothIcon className='h-4 w-4 md:h-6 md:w-6 text-gray-600 cursor-pointer' />
+        <BellIcon className='h-4 w-4 md:h-6 md:w-6 text-gray-600 cursor-pointer' />
+        <div className='h-8 w-8 md:h-10 md:w-10 rounded-full bg-red-200'>
           {/* TODO: Profile Picture */}
         </div>
+      </div>
+
+      <div className='flex items-center gap-4 md:hidden'>
+        <Bars3Icon className='h-6 w-6 text-gray-600 cursor-pointer' />
       </div>
 
 
